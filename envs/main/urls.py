637a4cadcefabce6731a045729511ad6416318ad
Stanislas Signoud <stan@stanisoft.net>
2012-07-22 17:38:54 +0200

    Fixing url imports.

--- envs/main/urls.py
+++ envs/main/urls.py
@@ -9,13 +9,12 @@ urlpatterns = patterns('',
     # url(r'^$', 'webapps.views.home', name='home'),
     # url(r'^webapps/', include('webapps.foo.urls')),
 
+    url(r'^dradis/?', include('dradis.urls')),
+
+    url(r'^admin/doc/?', include('django.contrib.admindocs.urls')),
+
     # Uncomment the next line to enable the admin:
-    url(r'^dradis/', include('dradis.urls')),
+    url(r'^admin/?', include(admin.site.urls)),
 
-    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    url(r'^admin/', include(admin.site.urls)),
-
-    url(r'^.*', include('deck.urls')),
+    url(r'^', include('deck.urls')),
 )
